476dab01f40ceb01edaab79441c504ddd750a9bf
ManishPatidar <manishpatidar1180@gmail.com>
2025-03-19 15:28:00 +0530

    Create Repository

---
+++ src/main/java/com/testcase/test_case_management_system/TestCaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.testcase.test_case_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCaseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCaseManagementSystemApplication.class, args);
+	}
+
+}
